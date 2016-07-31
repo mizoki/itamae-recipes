5851486cf0e453fd339e1dfec0c489f6a98a9c81
mizoki <h.mizoki@gmail.com>
2016-07-31 10:42:47 +0900

    Install git and tig

--- cookbooks/commons/default.rb
+++ cookbooks/commons/default.rb
@@ -2,3 +2,15 @@
 
 # disable selinux
 include_recipe 'selinux::disabled'
+
+# install common packages
+package 'epel-release' if node[:platform] == 'redhat'
+
+packages = [
+  'git',
+  'tig'
+]
+
+packages.each do |pkg|
+  package pkg
+end
